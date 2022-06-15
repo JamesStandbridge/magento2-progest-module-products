fa75e6f3f55cc8197119fc3c111fd61b1994beb4
jamesstandbridge <james.standbridge.git@gmail.com>
2022-06-15 16:21:03 +0200

    merge

--- Service/JobHandler.php
+++ Service/JobHandler.php
@@ -42,7 +42,11 @@ class JobHandler
             // if(!$rawProduct["prix"] || strlen(trim($rawProduct["prix"])) === 0) {
             //     $this->logger->info(sprintf("Can't create product sku = %s because his price is null",  $rawProduct["code_article"]));
             //     continue;
-            // }
+            // }           
+            if(!$rawProduct["nom"] || strlen(trim($rawProduct["nom"])) === 0) {
+                $this->logger->info(sprintf("Can't create product sku = %s because his name is null",  $rawProduct["code_article"]));
+                continue;
+            }
             try {
                 $product = $this->productFactory->createOrUpdate(
                     $rawProduct["code_article"],
@@ -74,17 +78,14 @@ class JobHandler
                 ->setAttributeValue($product, "categorie", $rawProduct["calibre"])
                 ->setAttributeValue($product, "ean_sku", $rawProduct["sku"])
                 ->setAttributeValue($product, "status", $rawProduct["status"])
+                ->setAttributeValue($product, "bio_arbo", $rawProduct["bio_arbo"])
+                ->setAttributeValue($product, "progest_arbo", $rawProduct["progest_arbo"])
                 ->setPrice($product, $rawProduct["prix"])
                 ->setQuantity($product, $rawProduct["quantite"])
             ;
 
             try {
-            
                 $product->save();
-
-                // dump($product->getId());
-                // dump($product->getSku());
-                // dump($product->getName());
 
                 $this->logger->info(sprintf("New product added to the catalog sku = %s", $product->getSku()));
                 $products_processed++;
@@ -93,7 +94,6 @@ class JobHandler
                 $this->logger->info(sprintf("Error while creating the product %s. %s", 
                 $product->getSku(), $e->getMessage()));
             }
-
         }
 
         return $products_processed;
@@ -135,7 +135,10 @@ class JobHandler
                 ->setAttributeValue($product, "classe_tva", $rawProduct["classe_tva"])
                 ->setAttributeValue($product, "ean_sku", $rawProduct["sku"])
                 ->setAttributeValue($product, "status", $rawProduct["status"])
+                ->setAttributeValue($product, "bio_arbo", $rawProduct["bio_arbo"])
+                ->setAttributeValue($product, "progest_arbo", $rawProduct["progest_arbo"])
             ;
+
 
 
             try {
